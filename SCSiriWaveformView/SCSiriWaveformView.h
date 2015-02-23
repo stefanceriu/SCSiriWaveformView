622c3239e78fad899f9c96065823abfa66fece72
Nicolas VERINAUD <nicolas@chloro.eu>
2015-02-23 10:13:18 +0100

    Update codestyle and add IBDesignable, IBInspectable to properties.
    - Convert tabs to spaces
    - Mark SCSiriWaveFormView as IB_DESIGNABLE to have a live preview in Interface Builder
    - Mark SCSiriWaveFormView properties as IBInspectable to allow configuration within Interface Builder.

--- SCSiriWaveformView/SCSiriWaveformView.h
+++ SCSiriWaveformView/SCSiriWaveformView.h
@@ -6,12 +6,13 @@
 //  Copyright (c) 2014 Stefan Ceriu. All rights reserved.
 //
 
+IB_DESIGNABLE
 @interface SCSiriWaveformView : UIView
 
 /*
  * Tells the waveform to redraw itself using the given level (normalized value)
  */
--(void)updateWithLevel:(CGFloat)level;
+- (void)updateWithLevel:(CGFloat)level;
 
 /*
  * The total number of waves
@@ -23,49 +24,49 @@
  * Color to use when drawing the waves
  * Default: white
  */
-@property (nonatomic, strong) UIColor *waveColor;
+@property (nonatomic, strong) IBInspectable UIColor *waveColor;
 
 /*
  * Line width used for the proeminent wave
  * Default: 3.0f
  */
-@property (nonatomic, assign) CGFloat primaryWaveLineWidth;
+@property (nonatomic, assign)  IBInspectable CGFloat primaryWaveLineWidth;
 
 /*
  * Line width used for all secondary waves
  * Default: 1.0f
  */
-@property (nonatomic, assign) CGFloat secondaryWaveLineWidth;
+@property (nonatomic, assign) IBInspectable CGFloat secondaryWaveLineWidth;
 
 /*
  * The amplitude that is used when the incoming amplitude is near zero.
  * Setting a value greater 0 provides a more vivid visualization.
  * Default: 0.01
  */
-@property (nonatomic, assign) CGFloat idleAmplitude;
+@property (nonatomic, assign) IBInspectable CGFloat idleAmplitude;
 
 /*
  * The frequency of the sinus wave. The higher the value, the more sinus wave peaks you will have.
  * Default: 1.5
  */
-@property (nonatomic, assign) CGFloat frequency;
+@property (nonatomic, assign) IBInspectable CGFloat frequency;
 
 /*
  * The current amplitude
  */
-@property (nonatomic, assign, readonly) CGFloat amplitude;
+@property (nonatomic, assign, readonly) IBInspectable CGFloat amplitude;
 
 /*
  * The lines are joined stepwise, the more dense you draw, the more CPU power is used.
  * Default: 5
  */
-@property (nonatomic, assign) CGFloat density;
+@property (nonatomic, assign) IBInspectable CGFloat density;
 
 /*
  * The phase shift that will be applied with each level setting
  * Change this to modify the animation speed or direction
  * Default: -0.15
  */
-@property (nonatomic, assign) CGFloat phaseShift;
+@property (nonatomic, assign) IBInspectable CGFloat phaseShift;
 
 @end
--- SCSiriWaveformView/SCSiriWaveformView.m
+++ SCSiriWaveformView/SCSiriWaveformView.m
@@ -8,6 +8,15 @@
 
 #import "SCSiriWaveformView.h"
 
+static const CGFloat kDefaultFrequency          = 1.5f;
+static const CGFloat kDefaultAplitude           = 1.0f;
+static const CGFloat kDefaultIdleAmplitude      = 0.01f;
+static const CGFloat kDefaultNumberOfWaves      = 5.0f;
+static const CGFloat kDefaultPhaseShift         = -0.15f;
+static const CGFloat kDefaultDensity            = 5.0f;
+static const CGFloat kDefaultPrimaryLineWidth   = 3.0f;
+static const CGFloat kDefaultSecondaryLineWidth = 1.0f;
+
 @interface SCSiriWaveformView ()
 
 @property (nonatomic, assign) CGFloat phase;
@@ -17,69 +26,70 @@
 
 @implementation SCSiriWaveformView
 
-- (id)init
+- (instancetype)init
 {
-    if(self = [super init]) {
-        [self setup];
-    }
-    
-    return self;
+	if(self = [super init]) {
+		[self setup];
+	}
+	
+	return self;
 }
 
-- (id)initWithFrame:(CGRect)frame
+- (instancetype)initWithFrame:(CGRect)frame
 {
-    if (self = [super initWithFrame:frame]) {
-        [self setup];
-    }
-    
-    return self;
+	if (self = [super initWithFrame:frame]) {
+		[self setup];
+	}
+	
+	return self;
 }
 
 - (void)awakeFromNib
 {
-    [self setup];
+	[super awakeFromNib];
+	
+	[self setup];
 }
 
 - (void)setup
 {
-    self.frequency = 1.5f;
-    
-    self.amplitude = 1.0f;
-    self.idleAmplitude = 0.01f;
-    
-    self.numberOfWaves = 5;
-    self.phaseShift = -0.15f;
-    self.density = 5.0f;
-    
-    self.waveColor = [UIColor whiteColor];
-    self.primaryWaveLineWidth = 3.0f;
-    self.secondaryWaveLineWidth = 1.0f;
+	self.waveColor = [UIColor whiteColor];
+	
+	self.frequency = kDefaultFrequency;
+	
+	self.amplitude = kDefaultAplitude;
+	self.idleAmplitude = kDefaultIdleAmplitude;
+	
+	self.numberOfWaves = kDefaultNumberOfWaves;
+	self.phaseShift = kDefaultPhaseShift;
+	self.density = kDefaultDensity;
+	
+	self.primaryWaveLineWidth = kDefaultPrimaryLineWidth;
+	self.secondaryWaveLineWidth = kDefaultSecondaryLineWidth;
 }
 
--(void)updateWithLevel:(CGFloat)level
+- (void)updateWithLevel:(CGFloat)level
 {
-    self.phase += self.phaseShift;
-    self.amplitude = fmax( level, self.idleAmplitude);
-    
-    [self setNeedsDisplay];
+	self.phase += self.phaseShift;
+	self.amplitude = fmax(level, self.idleAmplitude);
+	
+	[self setNeedsDisplay];
 }
 
 // Thanks to Raffael Hannemann https://github.com/raffael/SISinusWaveView
 - (void)drawRect:(CGRect)rect
 {
-    
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    CGContextClearRect(context, self.bounds);
-    
-    [self.backgroundColor set];
-    CGContextFillRect(context, rect);
-    
+	CGContextRef context = UIGraphicsGetCurrentContext();
+	CGContextClearRect(context, self.bounds);
+	
+	[self.backgroundColor set];
+	CGContextFillRect(context, rect);
+	
 	// We draw multiple sinus waves, with equal phases but altered amplitudes, multiplied by a parable function.
-	for(int i=0; i < self.numberOfWaves; i++) {
+	for (int i = 0; i < self.numberOfWaves; i++) {
+		CGContextRef context = UIGraphicsGetCurrentContext();
 		
-        CGContextRef context = UIGraphicsGetCurrentContext();
-        
-		CGContextSetLineWidth(context, (i==0 ? self.primaryWaveLineWidth : self.secondaryWaveLineWidth));
+		CGContextSetLineWidth(context, (i == 0 ? self.primaryWaveLineWidth : self.secondaryWaveLineWidth));
 		
 		CGFloat halfHeight = CGRectGetHeight(self.bounds) / 2.0f;
 		CGFloat width = CGRectGetWidth(self.bounds);
@@ -91,22 +101,20 @@
 		CGFloat progress = 1.0f - (CGFloat)i / self.numberOfWaves;
 		CGFloat normedAmplitude = (1.5f * progress - 0.5f) * self.amplitude;
 		
-        CGFloat multiplier = MIN(1.0, (progress / 3.0f * 2.0f) + (1.0f / 3.0f));
-        [[self.waveColor colorWithAlphaComponent:multiplier * CGColorGetAlpha(self.waveColor.CGColor)] set];
+		CGFloat multiplier = MIN(1.0, (progress / 3.0f * 2.0f) + (1.0f / 3.0f));
+		[[self.waveColor colorWithAlphaComponent:multiplier * CGColorGetAlpha(self.waveColor.CGColor)] set];
 		
-		for(CGFloat x = 0; x<width + self.density; x += self.density) {
-			
+		for (CGFloat x = 0; x<width + self.density; x += self.density) {
 			// We use a parable to scale the sinus wave, that has its peak in the middle of the view.
 			CGFloat scaling = -pow(1 / mid * (x - mid), 2) + 1;
-						
+			
 			CGFloat y = scaling * maxAmplitude * normedAmplitude * sinf(2 * M_PI *(x / width) * self.frequency + self.phase) + halfHeight;
 			
-			if (x==0) {
-                CGContextMoveToPoint(context, x, y);
-            }
-			else {
-                CGContextAddLineToPoint(context, x, y);
-            }
+			if (x == 0) {
+				CGContextMoveToPoint(context, x, y);
+			} else {
+				CGContextAddLineToPoint(context, x, y);
+			}
 		}
 		
 		CGContextStrokePath(context);
